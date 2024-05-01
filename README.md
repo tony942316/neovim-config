fa092999615ef157ba2a5af1ab0a94a1ebc581a6
tony942316 <grassoa159@gmail.com>
2024-05-01 05:26:23 +0000

    Added Ubuntu 24.04, README, and Package Updates

---
+++ README.md
@@ -0,0 +1,3 @@
+# Neovim Config
+
+## Contents
---
+++ scripts/ubuntu/24.04/install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Get Dependencies
+sudo apt install -y gcc g++ gcc-14 g++-14 git make ninja-build clang zip unzip \
+    curl tree ripgrep python3-venv npm cmake neovim
+
+mkdir $HOME/.config
+
+git config --global init.defaultBranch main
+git config --global user.email "grassoa159@gmail.com"
+git config --global user.name "tony942316"
+
+git clone https://github.com/tony942316/neovim-config.git $HOME/.config/nvim
+
+cd $HOME/.config/nvim
+git remote set-url origin git@github.com:tony942316/neovim-config.git
+
