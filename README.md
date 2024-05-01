3e814a8fdf6fe140ab09ed7304b30cda89e038cd
tony942316 <grassoa159@gmail.com>
2024-05-01 10:28:18 +0000

    Fix installer

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
 # Neovim Config
 
+Custom neovim configuration
+
 ## Contents
+
+1. [Ubuntu](#ubuntu)
+
+## Ubuntu Setup <a name="ubuntu"></a>
+
+### Ubuntu 24.04 LTS
+Download and run installer
+```
+cd $HOME
+wget https://raw.githubusercontent.com/tony942316/neovim-config/main/scripts/ubuntu/24.04/install.sh
+chmod +x install.sh
+./install.sh
+```
--- scripts/ubuntu/24.04/install.sh
+++ scripts/ubuntu/24.04/install.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
 # Get Dependencies
-sudo apt install -y gcc g++ gcc-14 g++-14 git make ninja-build clang zip unzip \
-    curl tree ripgrep python3-venv npm cmake neovim clangd clang-tools
+sudo apt install -y \
+    gcc g++ gcc-14 g++-14 \
+    git make ninja-build cmake \
+    clang clangd clang-tools \
+    zip unzip curl tree ripgrep \
+    python3-venv npm neovim
 
 mkdir $HOME/.config
 
@@ -10,8 +14,9 @@ git config --global init.defaultBranch main
 git config --global user.email "grassoa159@gmail.com"
 git config --global user.name "tony942316"
 
+git config --global alias.supdate 'submodule update --remote --merge'
+
 git clone https://github.com/tony942316/neovim-config.git $HOME/.config/nvim
 
 cd $HOME/.config/nvim
 git remote set-url origin git@github.com:tony942316/neovim-config.git
-
